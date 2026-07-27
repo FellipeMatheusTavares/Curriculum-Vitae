4495a2c304bb999b47d67658821e428c1976a170
FellipeTavares <97061679+FellipeMatheusTavares@users.noreply.github.com>
2026-07-27 17:16:41 -0300

    Add files via upload

---
+++ Portfolio/dados.js
@@ -0,0 +1,103 @@
+/*
+ESTRUTURA DO ARQUIVO
+
+1. Perfil
+2. Sobre
+3. Experiências
+4. Formação
+5. Habilidades
+6. Projetos
+7. Certificados
+8. Idiomas
+9. Contato
+*/
+
+const dados = {
+
+    perfil: {
+
+        nome: "Fellipe Matheus Tavares Silva",
+        cargo: "Analista de Dados",
+        foto: "assets/img/perfil.jpg",
+        curriculo: "assets/curriculo.pdf"
+
+    },
+
+    sobre: {
+
+        descricao: "Data Analyst com formação em Ciência da Computação e experiência em análise, visualização e automação de dados. Atuação em órgãos públicos com foco em otimização de fluxos, Python, SQL e Power Platform, atuando em análise, visualização e automação de dados.",
+
+    },
+
+    experiencias: [
+
+        {
+            empresa: "Secretaria Nacional de Trânsito - SENATRAN",
+            cargo: "Assistente Administrativo",
+            periodo: "2024 - Atual",
+            descricao: "Descrição das atividades desenvolvidas."
+        },
+
+        {
+            empresa: "Agência Nacional de Aviação Civil - ANAC",
+            cargo: "Estagiário",
+            periodo: "2022 - 2024",
+            descricao: "Descrição das atividades desenvolvidas."
+        }
+
+    ],
+
+
+    formacao: [
+        {
+            instituição: "Universidade Catolica de Brasilia - UCB",
+            Curso: "Ciencia da Computação",
+            nivel: "Graduação",
+            periodo: "2020 - 2024"
+        },
+        {
+            instituição: "Centro Universitário Internacional - UNINTER",
+            Curso: "Ciência de Dados e Inteligência Artificial",
+            nivel: "Pós-Graduação",
+            periodo: "2026"
+        },{
+            instituição: "Centro Universitário Internacional - UNINTER",
+            Curso: "Engenharia da Computação",
+            nivel: "Pós-Graduação",
+            periodo: "2026"
+        }
+            
+
+    ],
+
+
+    habilidades: [
+
+    ],
+
+
+    projetos: [
+
+    ],
+
+    //---------Ver se coloco o projeto do jogos e do formulario SENATRAN-----------------
+
+    certificados: [
+
+    ],
+
+
+    idiomas: [
+
+    ],
+
+    contato: {
+
+        email: "dev.fellipetavares@gmail.com",
+        telefone: "61999378846",
+        github: "https://github.com/FellipeMatheusTavares",
+        linkedin: "https://www.linkedin.com/in/fellipetavares17/"
+
+    }
+
+};
